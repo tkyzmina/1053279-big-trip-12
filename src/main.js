80b20d5f7092b003ac16eda731ee98441d5c08ff
tkyzmina <tkyzmina@gmail.com>
2020-08-18 21:00:52 +0300

    добавялет презентер и выводит список эвентов

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import SortView from "./view/sort.js";
 import NavView from "./view/nav.js";
 import FilterView from "./view/filter.js";
 import DaysListView from "./view/day-list.js";
-import DayView from "./view/day.js";
-import DayInfoView from "./view/day-info.js";
-import EventsListView from "./view/events-list.js";
-import EventView from "./view/event.js";
-import NewEventView from "./view/new-event.js";
-
+import EventsBoardPresenter from "./presenter/trip.js";
 
 import {
   generateEvent
@@ -17,7 +12,6 @@ import {
 import {
   render,
   RenderPosition,
-  replace
 } from "./utils/render.js";
 
 const EVENTS_COUNT = 15;
@@ -29,67 +23,15 @@ const headerMainInfo = headerElement.querySelector(`.trip-main`);
 const navElement = headerElement.querySelector(`.trip-controls`);
 
 const pageMainElement = document.querySelector(`.page-main`);
-const eventsElement = pageMainElement.querySelector(`.trip-events`);
+const eventsBoardElement = pageMainElement.querySelector(`.trip-events`);
 
 render(headerMainInfo, new TripInfoView(events), RenderPosition.AFTERBEGIN);
 render(navElement, new NavView(), RenderPosition.AFTERBEGIN);
 render(navElement, new FilterView(), RenderPosition.BEFOREEND);
-render(eventsElement, new SortView(), RenderPosition.AFTERBEGIN);
+render(eventsBoardElement, new SortView(), RenderPosition.BEFOREEND);
 
 const daysListComponent = new DaysListView();
-render(eventsElement, daysListComponent, RenderPosition.BEFOREEND);
+render(eventsBoardElement, daysListComponent, RenderPosition.BEFOREEND);
 
-const dayComponent = new DayView();
-render(daysListComponent, dayComponent, RenderPosition.BEFOREEND);
-render(dayComponent, new DayInfoView(), RenderPosition.AFTERBEGIN);
-
-const eventsListComponent = new EventsListView();
-render(dayComponent, eventsListComponent, RenderPosition.BEFOREEND);
-
-const renderEvent = (eventList, event) => {
-  const eventComponent = new EventView(event);
-  const eventEditComponent = new NewEventView(event);
-
-  const replaceEventToNewEvent = () => {
-    replace(eventEditComponent, eventComponent);
-  };
-  const replaceNewEventToEvent = () => {
-    replace(eventComponent, eventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape` || evt.key === `Esc`) {
-      evt.preventDefault();
-      replaceNewEventToEvent();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-      document.removeEventListener(`click`, onCancel);
-    }
-  };
-
-  const onCancel = (evt) => {
-    const cancelBtn = document.querySelector(`.event__reset-btn`);
-    if (evt.target === cancelBtn) {
-      replaceNewEventToEvent();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-      document.removeEventListener(`click`, onCancel);
-    }
-  };
-
-  eventComponent.setEditClickHandler(() => {
-    replaceEventToNewEvent();
-    document.addEventListener(`keydown`, onEscKeyDown);
-    document.addEventListener(`click`, onCancel);
-  });
-
-  eventEditComponent.setFormSubmitHandler(() => {
-    replaceNewEventToEvent();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-    document.removeEventListener(`click`, onCancel);
-  });
-
-  render(eventList, eventComponent, RenderPosition.BEFOREEND);
-};
-
-for (let i = 0; i < EVENTS_COUNT; i++) {
-  renderEvent(eventsListComponent, events[i]);
-}
+const tripPresenter = new EventsBoardPresenter(daysListComponent);
+tripPresenter.init(events);
---
+++ src/presenter/trip.js
@@ -0,0 +1,83 @@
+import DayView from "../view/day.js";
+import DayInfoView from "../view/day-info.js";
+import EventsListView from "../view/events-list.js";
+import EventView from "../view/event.js";
+import NewEventView from "../view/new-event.js";
+
+import {
+  render,
+  RenderPosition,
+  replace
+} from "../utils/render.js";
+const EVENTS_COUNT = 15;
+export default class Trip {
+  constructor(container) {
+    this._container = container;
+    this._dayComponent = new DayView();
+    this._dayInfoComponent = new DayInfoView();
+    this._eventsListComponent = new EventsListView();
+    this._eventComponent = new EventView();
+    this._newEventComponent = new NewEventView();
+  }
+
+  init(events) {
+    this._events = events.slice();
+
+    render(this._container, this._dayComponent, RenderPosition.BEFOREEND);
+    render(this._dayComponent, this._dayInfoComponent, RenderPosition.AFTERBEGIN);
+    render(this._dayComponent, this._eventsListComponent, RenderPosition.BEFOREEND);
+
+    this._renderEvents(events);
+  }
+
+  _renderEvent(event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new NewEventView(event);
+
+    const replaceEventToNewEvent = () => {
+      replace(eventEditComponent, eventComponent);
+    };
+    const replaceNewEventToEvent = () => {
+      replace(eventComponent, eventEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceNewEventToEvent();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+        document.removeEventListener(`click`, onCancel);
+      }
+    };
+
+    const onCancel = (evt) => {
+      const cancelBtn = document.querySelector(`.event__reset-btn`);
+      if (evt.target === cancelBtn) {
+        replaceNewEventToEvent();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+        document.removeEventListener(`click`, onCancel);
+      }
+    };
+
+    eventComponent.setEditClickHandler(() => {
+      replaceEventToNewEvent();
+      document.addEventListener(`keydown`, onEscKeyDown);
+      document.addEventListener(`click`, onCancel);
+    });
+
+    eventEditComponent.setFormSubmitHandler(() => {
+      replaceNewEventToEvent();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+      document.removeEventListener(`click`, onCancel);
+    });
+
+    render(this._eventsListComponent, eventComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderEvents(events) {
+    for (let i = 0; i < EVENTS_COUNT; i++) {
+      this._renderEvent(events[i]);
+    }
+  }
+
+}
